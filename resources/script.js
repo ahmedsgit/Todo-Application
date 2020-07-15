54a51611374cbf7e1b8e7b015da274c9bef05efa
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-15 11:56:48 +0600

    Fade in effect

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,6 @@
 //add Button targeted
 var addButton = document.getElementById('add');
-addButton.addEventListener('click', addItems);
+addButton.addEventListener('click', addItem);
 
 //remove Button targeted
 var removeButton = document.getElementById('remove');
@@ -15,8 +15,39 @@ var ul = document.getElementById('list');
 var li;
 
 // Functions
-function addItems() {
-    console.log('Add');
+function addItem() {
+    //var input = document.getElementById('input');
+    var item = input.value;
+    var textNode = document.createTextNode(item);
+
+    if (item === '') {
+        return false;
+    } else {
+        // create li
+        li = document.createElement('li');
+
+        //create input, type(checked),id(check)
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.setAttribute('id', 'check');
+
+        //create label
+        var label = document.createElement('label');
+        label.setAttribute('for', 'check');
+
+        //add these elements on webpage 
+        li.appendChild(checkbox);
+        label.appendChild(textNode);
+        li.appendChild(label);
+        ul.insertBefore(li, ul.childNodes[0]);
+
+        //fade in
+        setTimeout(() => {
+            li.className = 'visual';
+        }, 5);
+
+        input.value = '';
+    }
 }
 
 function removeItems() {
